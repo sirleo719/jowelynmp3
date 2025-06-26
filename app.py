a2f058aef9114f9f72c88bb3a49ba7460874a28a
sirleo719 <sirleo719@gmail.com>
2025-06-26 16:06:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,46 @@
-from flask import Flask, render_template_string, request, send_file
-import yt_dlp
-import os
+from flask import Flask, render_template_string, request
 
 app = Flask(__name__)
-DOWNLOAD_DIR = "downloads"
-os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 HTML = '''
 <!DOCTYPE html>
 <html>
 <head>
-    <title>jowelynmp3</title>
+    <title>🎵 jowelynmp3 🎵</title>
 </head>
-<body>
-    <div style="text-align: center;">
-        <h2>🎵 Welcome to jowelynmp3 🎵</h2>
-        <form method="post">
-            Enter YouTube URL: <input type="text" name="url" required>
-            <input type="submit" value="Download MP3">
-        </form>
-        {% if filename %}
-            <p>Your MP3 is ready: <a href="/download/{{ filename }}">Click here to download</a></p>
-        {% elif error %}
-            <p style="color: red;">Error: {{ error }}</p>
-        {% endif %}
-    </div>
+<body style="text-align: center; font-family: sans-serif; padding-top: 50px;">
+    <h1>🎵 Welcome to jowelynmp3 🎵</h1>
+    <form method="POST">
+        <input type="text" name="url" placeholder="Enter YouTube URL" size="50" required>
+        <br><br>
+        <input type="submit" value="Convert to MP3">
+    </form>
+
+    {% if video_id %}
+        <h2>Your MP3 is ready:</h2>
+        <iframe src="https://api.vevioz.com/api/button/mp3/{{ video_id }}" width="100%" height="60" frameborder="0" allowtransparency="true" scrolling="no" style="border:none;"></iframe>
+    {% endif %}
 </body>
 </html>
 '''
 
+def extract_video_id(url):
+    if "watch?v=" in url:
+        return url.split("watch?v=")[1].split("&")[0]
+    elif "youtu.be/" in url:
+        return url.split("youtu.be/")[1].split("?")[0]
+    return None
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    filename = None
-    error = None
+    video_id = None
     if request.method == 'POST':
         url = request.form['url']
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'outtmpl': f'{DOWNLOAD_DIR}/%(title)s.%(ext)s',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-        }
-        try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                info = ydl.extract_info(url, download=True)
-                filename = f"{info['title']}.mp3"
-        except Exception as e:
-            error = str(e)
-    return render_template_string(HTML, filename=filename, error=error)
-
-@app.route('/download/<filename>')
-def download(filename):
-    return send_file(f"{DOWNLOAD_DIR}/{filename}", as_attachment=True)
+        video_id = extract_video_id(url)
+    return render_template_string(HTML, video_id=video_id)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
-    app.run(host='0.0.0.0', port=port, debug=False)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
     
